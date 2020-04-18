86a062a14e1906dabd60012b57664d6170d9a847
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2020-04-17 23:51:32 -0300

    Adds build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
-#/bin/bash
-
-set -eu -o pipefail
-
-DEST_DIR='/tmp/hugo-build'
-FILES_TO_REMOVE='404.html  apple-touch-icon.png  browserconfig.xml
-CNAME  custom  favicon-16x16.png  favicon.ico  index.html  js
-mstile-150x150.png  robots.txt sitemap.xml about blog categories
-css favicon-32x32.png img index.xml manifest.json projects safari-pinned-tab.svg  tags'
-
-hugo -d ${DEST_DIR}
-
+#!/bin/bash -exu
+mv "public/" "${HOME}/" 
 git checkout master
-sleep 1
-rm -rf ${FILES_TO_REMOVE}
 
-mv -fv ${DEST_DIR}/* .
+if [[ -d .git ]]; then
+  rm -rfv *
+  mv ${HOME}/public/* . 
+  git add -A && git commit -m "New version" && git push -f
+fi
 
-git add -A
+git checkout develop
