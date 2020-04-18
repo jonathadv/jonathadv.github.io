a62d9b3a9be105c3d2ce7f7c1fb05cf85b754e53
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2020-04-17 23:57:22 -0300

    Adds CNAME to buildsh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -exu
 hugo
 mv "public/" "${HOME}/" 
+cp CNAME "${HOME}/" 
 git checkout master
 
 if [[ -d .git ]]; then
   rm -rfv *
   mv ${HOME}/public/* . 
+  mv "${HOME}/CNAME"  .
   git add -A && git commit -m "New version" && git push -f
 fi
 
